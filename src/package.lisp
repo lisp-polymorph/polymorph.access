9e929004b784e8a01916e13ea58cb0f7b6eaaaa1
Andrey Sokolov <aun.sokolov@gmail.com>
2021-05-21 18:53:56 +0300

    Renaming (no adhoc)

--- src/package.lisp
+++ src/package.lisp
@@ -1,7 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:polymorph.access
-  (:use #:cl #:adhoc-polymorphic-functions #:alexandria #:polymorph.utility)
+  (:use #:cl #:polymorphic-functions #:alexandria #:polymorph.utility)
   (:local-nicknames (:cm :sandalphon.compiler-macro))
   (:shadow #:emptyp)
   (:export #:at
